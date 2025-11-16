f1641e14ce7ab169a46101a2002bfa4c7d895dd3
sharp-lilac <jessicacboles@gmail.com>
2025-11-16 16:44:49 -0600

    Create bar plots for STEM Fair 2025

--- 2025 - STEM Fair/stem_fair_2025.R
+++ 2025 - STEM Fair/stem_fair_2025.R
@@ -4,6 +4,8 @@
 library(tidyverse)
 library(networkD3)
 library(htmlwidgets)
+library(ggpubr)
+library(ggmosaic)
 
 # Load data ---------------------------
 df = read.csv("2025 11 14 - Environmental Solutions - UB STEM Fair.csv")
@@ -50,3 +52,41 @@ plot_sankey <- sankeyNetwork(
   nodeWidth = 70
 )
 saveWidget(plot_sankey, "sankey_diagram.html")
+
+# Strip concerns and solutions ---------------------------
+df_concerns <- df %>%
+  mutate(Concern_Category = str_split(Concern_Category, ", ")) %>%
+  unnest(Concern_Category) %>%
+  group_by(Concern_Category, Respondent_Type) %>%
+  summarize(Total = n()) %>%
+  filter(Total >2) %>% 
+  arrange(desc(Total)) %>%
+  ungroup() %>%
+  mutate(Concern_Category = factor(Concern_Category, levels = unique(Concern_Category), ordered = TRUE),
+         Respondent_Type = factor(Respondent_Type, levels = c("UB Student", "Other Student", "UB Staff/Faculty"), ordered = TRUE))
+df_solutions <- df %>%
+  mutate(Solution_Category = str_split(Solution_Category, ", ")) %>%
+  unnest(Solution_Category) %>%
+  group_by(Solution_Category, Respondent_Type) %>%
+  summarize(Total = n()) %>%
+  filter(Total >3) %>% 
+  arrange(desc(Total)) %>%
+  ungroup() %>%
+  mutate(
+    Solution_Category = factor(Solution_Category, levels = unique(Solution_Category), ordered = TRUE),
+    Respondent_Type = factor(Respondent_Type, levels = c("UB Student", "Other Student", "UB Staff/Faculty"), ordered = TRUE)
+    )
+
+# Create bar plots ---------------------------
+plot_bar_concerns <- ggplot(df_concerns, aes(x = Concern_Category, y = Total, fill = Respondent_Type)) + 
+  geom_col() +
+  scale_fill_manual(values = palette) +
+  theme_pubclean() +
+  labs(x = "Concern", fill = "Respondent Type")
+plot_bar_solutions <- ggplot(df_solutions, aes(x = Solution_Category, y = Total, fill = Respondent_Type)) + 
+  geom_col() +
+  scale_fill_manual(values = palette) +
+  theme_pubclean() +
+  labs(x = "Solution", fill = "Respondent Type")
+ggsave(filename = "stem_fair_2025_plot_bar_concerns.png", width = 14, height = 6, plot_bar_concerns)
+ggsave(filename = "stem_fair_2025_plot_bar_solutions.png", width = 14, height = 6, plot_bar_solutions)
