412c850d8314bc686ad615906efcb1673d98eadb
sharp-lilac <jessicacboles@gmail.com>
2025-11-16 12:49:23 -0600

    Create stem_fair_2025.R
    Initializing the file, loading data, adding palette

---
+++ 2025 - STEM Fair/stem_fair_2025.R
@@ -0,0 +1,17 @@
+## stem_fair_2025.R
+
+# Load packages ---------------------------
+library(tidyverse)
+
+# Load data ---------------------------
+df = read.csv("2025 11 14 - Environmental Solutions - UB STEM Fair.csv")
+
+# Define color palette ---------------------------
+palette <- c(
+  "#5a7494", "#b4c781", "#88C0D0", "#B48EAD", "#4C566A", "#b69efa",
+  "#ce4fa4", "#e7a042", "#FB9A99", "#0d8086", "#79cc92", "#7E3793",
+  "#33A02C", "#c35e68", "#eaea9a", "#9933FF", "#999933",
+  "#FFCCFF", "#336600", "#999999", "#990000", "#f5f5f5ff",
+  "#E22828", "coral", "#eeeec2", "violet", "#a5652a", "#d6fdfd",
+  "#fcfc00", "#b69efa", "#ffecc8", "#3b3ba5"
+)
