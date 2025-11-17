9e5c37a7cb315cd620b57f051507766443fd086c
sharp-lilac <jessicacboles@gmail.com>
2025-11-17 08:27:36 -0600

    Improve theming for STEM Fair 2025 figures

--- 2025 - STEM Fair/stem_fair_2025.R
+++ 2025 - STEM Fair/stem_fair_2025.R
@@ -7,6 +7,17 @@ library(htmlwidgets)
 library(ggpubr)
 library(ggmosaic)
 
+# Define theming ---------------------------
+palette <- c(
+  "#5a7494", "#b4c781", "#88C0D0", "#B48EAD", "#4C566A", "#b69efa",
+  "#ce4fa4", "#e7a042", "#FB9A99", "#0d8086", "#79cc92", "#7E3793",
+  "#33A02C", "#c35e68", "#eaea9a", "#9933FF", "#999933",
+  "#FFCCFF", "#336600", "#999999", "#990000", "#f5f5f5ff",
+  "#E22828", "coral", "#eeeec2", "violet", "#a5652a", "#d6fdfd",
+  "#fcfc00", "#b69efa", "#ffecc8", "#3b3ba5"
+)
+my_theme <- theme(axis.title = element_text(size = 16), axis.text  = element_text(size = 13))
+
 # Load data ---------------------------
 df = read.csv("2025 11 14 - Environmental Solutions - UB STEM Fair.csv")
 
@@ -82,11 +93,13 @@ plot_bar_concerns <- ggplot(df_concerns, aes(x = Concern_Category, y = Total, fi
   geom_col() +
   scale_fill_manual(values = palette) +
   theme_pubclean() +
-  labs(x = "Concern", fill = "Respondent Type")
+  labs(x = "Concern", fill = "Respondent Type") +
+  my_theme
 plot_bar_solutions <- ggplot(df_solutions, aes(x = Solution_Category, y = Total, fill = Respondent_Type)) + 
   geom_col() +
   scale_fill_manual(values = palette) +
   theme_pubclean() +
-  labs(x = "Solution", fill = "Respondent Type")
+  labs(x = "Solution", fill = "Respondent Type") +
+  my_theme
 ggsave(filename = "stem_fair_2025_plot_bar_concerns.png", width = 14, height = 6, plot_bar_concerns)
 ggsave(filename = "stem_fair_2025_plot_bar_solutions.png", width = 14, height = 6, plot_bar_solutions)
